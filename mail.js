ba44dc9aac2bd9edc4ec979a02a489f38655806a
James Schaffer <jas.schaffer@gmail.com>
2022-02-13 09:35:37 -0500

    mail.js

---
+++ mail.js
@@ -0,0 +1,28 @@
+import 'dotenv/config'
+import nodemailer from 'nodemailer'
+import {from, to, subject, textBody, htmlBody} from './email_template.js'
+
+async function main() {
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    auth: {
+        user: process.env.USERNAME,
+        pass: process.env.PASS
+    }
+  });
+  
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    textBody,
+    htmlBody
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+main().catch(console.error);
