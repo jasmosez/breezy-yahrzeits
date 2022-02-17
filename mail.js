a2ea876e25045b6241ebe993a218cc507b342318
James Schaffer <jas.schaffer@gmail.com>
2022-02-16 22:31:04 -0500

    add cc to mail

--- mail.js
+++ mail.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import nodemailer from 'nodemailer'
-import {from, to, subject, textBody, htmlBody} from './email_template.js'
+import {from, to, cc, subject, textBody, htmlBody} from './email_template.js'
 
 async function main() {
 
@@ -17,6 +17,7 @@ async function main() {
   let info = await transporter.sendMail({
     from,
     to,
+    cc,
     subject,
     textBody,
     htmlBody
