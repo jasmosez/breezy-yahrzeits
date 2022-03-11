b6a85d32d3c222a34b0b7480ae335808596967e9
james <james.schaffer@datasociety.com>
2022-03-11 12:08:17 -0500

    move run to lib

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-import {getInput, 
-        fetchYahrzeitFormEntries, 
-        loadYahrzeitFormEntries, 
-        filterResponses, 
-        saveToCSV} from './lib/dataCollection.js'
-
-const run = async () => {
-    const monthFilter = getInput()
-    if (!monthFilter) {
-        console.log('Entry is not a number in range. Exiting.')
-        process.exit()
-    }
-    const json = await fetchYahrzeitFormEntries()
-    // const json = await loadYahrzeitFormEntries()
-    saveToCSV(filterResponses(json))
-    console.log(`Filter month = ${monthFilter}`)
-
-}
+import {run} from './lib/dataCollection.js'
 
 run()
 
