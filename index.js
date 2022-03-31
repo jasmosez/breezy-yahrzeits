db9797a6161d797d445933c95f26c1518fe8f6cb
james <james.schaffer@datasociety.com>
2022-03-31 12:34:30 -0400

    Ibid

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import {getInput,
         loadYahrzeitFormEntries, 
         mapResponses, 
         saveToCSV} from './lib/dataCollection.js'
-// import {}
+import {determineTargetYear, makeTextData, saveToText} from './lib/weeklyList.js'
+import {MONTHS} from './lib/util.js'
 import { EMAIL, ENGLISH_NAME_DECEASED, FIRST_NAME_MOURNER, PROFILE, RELATIONSHIP } from './lib/form_constants.js'
+import promptInit from "prompt-sync"
+
 
 const loggingFilteredForms = (filteredForms) => {
     filteredForms.forEach(form => console.log(`
@@ -27,29 +30,59 @@ const run = async () => {
         console.log('Entry is not a number in range. Exiting.')
         process.exit()
     }
+
+    const targetYear = determineTargetYear(monthFilter)
+    console.log('Getting yahrzeits occurring in:')
+    console.log(`${MONTHS[monthFilter -1].toUpperCase()} ${targetYear}`)
+    console.log('')
+
+
     const json = await fetchYahrzeitFormEntries()
-    console.log(json)
+    // const json = await loadYahrzeitFormEntries()
     // NEED ERROR HANDLING
     
-    // const json = await loadYahrzeitFormEntries()
     
     //filtered
     const filteredForms = mapResponses(json).filter(form => form.response.month_number === monthFilter)
-    loggingFilteredForms(filteredForms)
+    // loggingFilteredForms(filteredForms)
 
     
     // dump text for shabbat email
-    //0. objects with mourner, realationsip, deceased, yahr date
+    const textDataObj = makeTextData(monthFilter, filteredForms)
+    if (!textDataObj.success) {
+        throw 'Could not process yahrzeit text for shabbat email'
+    }
+    const savedTXT = saveToText(textDataObj.outputString)
     
-    //1. grouped by shabbat -> the preceding week?
-    //2. <Mourner>, <relationship> of <deceased> (<greg yahrzeit date>)
     
     
     // user check in 
     // 1. save everything to a PDF
-    saveToCSV(mapResponses(json))
+    const savedCSV = await saveToCSV(filteredForms)
     // 2. prompt user to review and continue with email or not
-    // 3. send emails
+    const prompt = promptInit()
+
+    console.log('_________________________________________________________________')
+    console.log(`CSV of Yahrzeit forms saved to:  ${savedCSV}`)
+    console.log(`Text for shabbat email saved to: ${savedTXT}`)
+    console.log(``)
+    console.log(`Please review both files.`)
+    console.log(`If everything looks right, we'll send the emails to the mourners.`)
+    const response = prompt(`Type "Send Emails" to confirm: `)
+    console.log(``)
+
+    if (response === 'Send Emails') {
+        console.log('_________________________________________________________________')
+        console.log('Sending Emails...')
+        // 3. send emails
+    } else {
+        console.log('_________________________________________________________________')
+        console.log('Not sending emails.')
+        console.log('Re-run script or contact support to address an error.')
+        console.log('_________________________________________________________________')
+        console.log('')
+    }
+
 
 
 
